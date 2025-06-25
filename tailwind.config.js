9a06502934fa98cc1bd98f1144cb199ef0e27065
Peter Gunary <pgunary@gmail.com>
2025-06-25 16:26:56 +0100

    Apply rustic family brand styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3B5F3B',
+        secondary: '#A97449',
+        accent: '#D5B57A',
+        background: '#F9F6F1',
+        contrast: '#2E2E2E'
+      },
+      fontFamily: {
+        heading: ['"Playfair Display"', 'serif'],
+        body: ['Lora', 'serif']
+      }
+    },
   },
   plugins: [],
 }
