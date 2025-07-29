6819a6cc9dd7af402d0cdb97d35e55bea417ef55
Peter Gunary <pgunary@gmail.com>
2025-07-29 15:07:59 +0100

    Add editable block components

--- vineyard-site/src/app/shared/shared-imports.ts
+++ vineyard-site/src/app/shared/shared-imports.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { QuillModule } from 'ngx-quill';
+import { ColorPickerDirective, ColorPickerComponent } from 'ngx-color-picker';
 
 export const SHARED_IMPORTS = [
-  CommonModule
+  CommonModule,
+  FormsModule,
+  QuillModule,
+  ColorPickerDirective,
+  ColorPickerComponent
 ];
