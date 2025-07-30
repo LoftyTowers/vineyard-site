b45792879a365633611bad13477f79114fa8e69b
Peter Gunary <pgunary@gmail.com>
2025-07-30 12:44:35 +0100

    install EF tool in build stage

--- VineyardApi/entrypoint.sh
+++ VineyardApi/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Apply database migrations if available
 if [ "$SKIP_MIGRATIONS" != "true" ]; then
-  dotnet ef database update --project ./VineyardApi/VineyardApi.csproj --no-build
+  dotnet ef database update --project ./src/VineyardApi.csproj --no-build
 fi
 
 exec dotnet VineyardApi.dll
