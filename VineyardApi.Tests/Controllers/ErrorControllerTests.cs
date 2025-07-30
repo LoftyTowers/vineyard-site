a8f0aefeacc60f8a32290677a8657e2ac956565b
Peter Gunary <pgunary@gmail.com>
2025-07-30 13:59:28 +0100

    Add tests for ErrorController

---
+++ VineyardApi.Tests/Controllers/ErrorControllerTests.cs
@@ -0,0 +1,75 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using VineyardApi.Controllers;
+
+namespace VineyardApi.Tests.Controllers
+{
+    public class ErrorControllerTests
+    {
+        private Mock<ILogger<ErrorController>> _logger = null!;
+        private ErrorController _controller = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = new Mock<ILogger<ErrorController>>();
+            _controller = new ErrorController(_logger.Object);
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+        }
+
+        [Test]
+        public void HandleStatusCode_404_ReturnsNotFoundMessage()
+        {
+            var result = _controller.HandleStatusCode(404);
+
+            var problem = (result as ObjectResult)?.Value as ProblemDetails;
+            problem.Should().NotBeNull();
+            problem!.Status.Should().Be(404);
+            problem!.Title.Should().Be("Resource not found");
+        }
+
+        [Test]
+        public void HandleStatusCode_Non404_ReturnsGenericMessage()
+        {
+            var result = _controller.HandleStatusCode(500);
+
+            var problem = (result as ObjectResult)?.Value as ProblemDetails;
+            problem.Should().NotBeNull();
+            problem!.Status.Should().Be(500);
+            problem!.Title.Should().Be("An unexpected error occurred");
+        }
+
+        [Test]
+        public void HandleException_LogsErrorAndReturnsProblem()
+        {
+            var httpContext = new DefaultHttpContext();
+            var exception = new System.Exception("oops");
+            var feature = new Mock<IExceptionHandlerFeature>();
+            feature.Setup(f => f.Error).Returns(exception);
+            httpContext.Features.Set(feature.Object);
+            _controller.ControllerContext = new ControllerContext { HttpContext = httpContext };
+
+            var result = _controller.HandleException();
+
+            _logger.Verify(l => l.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                exception,
+                It.IsAny<Func<It.IsAnyType, System.Exception?, string>>()),
+                Times.Once);
+
+            var problem = (result as ObjectResult)?.Value as ProblemDetails;
+            problem.Should().NotBeNull();
+            problem!.Status.Should().Be(500);
+        }
+    }
+}
