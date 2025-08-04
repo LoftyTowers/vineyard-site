aa63566aaf2b00fbbc643af32af138316507577d
Peter Gunary <pgunary@gmail.com>
2025-08-04 10:25:49 +0100

    test: add page service tests

---
+++ vineyard-site/src/app/services/page.service.spec.ts
@@ -0,0 +1,32 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { PageService, PageData } from './page.service';
+
+describe('PageService', () => {
+  let service: PageService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(PageService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('requests page data by route', () => {
+    const mockResponse: PageData = { blocks: [{ type: 'p', content: 'hi' }] } as any;
+
+    service.getPage('about').subscribe((data) => {
+      expect(data).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne('/api/pages/about');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+});
