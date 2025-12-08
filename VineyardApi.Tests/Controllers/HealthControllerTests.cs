c673364ead7edd3d6b48805ba573363d64fbb74e
Peter Gunary <pgunary@gmail.com>
2025-12-08 12:08:23 +0000

    Add HTTP status mapping coverage for controllers

--- VineyardApi.Tests/Controllers/HealthControllerTests.cs
+++ VineyardApi.Tests/Controllers/HealthControllerTests.cs
@@ -1,7 +1,9 @@
 using FluentAssertions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using VineyardApi.Controllers;
+using VineyardApi.Tests;
 
 namespace VineyardApi.Tests.Controllers
 {
@@ -21,6 +23,14 @@ namespace VineyardApi.Tests.Controllers
             var result = _controller.Get();
 
             result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Test]
+        public void Get_MapsToStatusCode200()
+        {
+            var result = _controller.Get();
+
+            ResultHttpMapper.MapToStatusCode(result).Should().Be(StatusCodes.Status200OK);
         }
     }
 }
